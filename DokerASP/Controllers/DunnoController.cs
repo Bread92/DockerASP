38407852677edb3ffbd427a6ccc60854dc1553c6
Bread92 <vaneck.van2019@gmail.com>
2024-03-08 01:55:14 +0300

    Add Controller CreateFile

---
+++ DokerASP/Controllers/DunnoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DokerASP.Controllers;
+
+[ApiController]
+public class DunnoController : ControllerBase
+{
+    [HttpPost("{filename}/{content}")]
+    public async Task<IActionResult> CreateFile([FromRoute] string filename, [FromRoute] string content)
+    {
+        const string outputDir = "/output";
+
+        Directory.CreateDirectory(outputDir);
+        
+        await using (StreamWriter sw = new(Path.Combine(outputDir, filename)))
+        {
+            await sw.WriteLineAsync(content);
+        }
+
+        return Ok();
+    }
+}
--- DokerASP/Program.cs
+++ DokerASP/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+services.AddControllers();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
+app.MapControllers();
 app.Run();
